4e1a83f68a3d5e40e5e9dd194d28895b6a3a7aa7
L-R-C-C-M <liuruimeng291@163.com>
2022-12-14 00:01:43 +0800

    Initial commit

---
+++ src/main/java/com/datawarehouse/datawarehouse_backend/DataWarehouseBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.datawarehouse.datawarehouse_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataWarehouseBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataWarehouseBackEndApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/datawarehouse/datawarehouse_backend/DataWarehouseBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.datawarehouse.datawarehouse_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataWarehouseBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
